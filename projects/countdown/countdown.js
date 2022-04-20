5a875c6bf7ef99d18a4226126aa0c7791da8269c
Dafni Hernandez <dafni74gx@gmail.com>
2022-04-19 21:36:48 -0400

    Added countdown

---
+++ projects/countdown/countdown.js
@@ -0,0 +1,16 @@
+var countDownDate = new Date("June 15, 2022 15:10:00").getTime();
+var countdownfunction = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  document.getElementById("timer").innerHTML = days + "d • " + hours + "h • " + minutes + "m • " + seconds + "s";
+   
+  if (distance < 0) {
+    clearInterval(countdownfunction);
+    document.getElementById("timer").innerHTML = "Get your shades on 😎";
+  }
+}, 1000);
